0d3609b11f42d3b236796f42bdfcb02351cca00b
Yukio MURAKAMI <m_yukio@mac.com>
2011-09-29 23:57:25 +0900

    Audio Unit

--- DemoAudio/AudioUnitViewController.m
+++ DemoAudio/AudioUnitViewController.m
@@ -8,6 +8,9 @@
 
 #import "AudioUnitViewController.h"
 
+static AudioStreamBasicDescription AUCanonicalASBD(Float64 sampleRate, UInt32 channel);
+static AudioStreamBasicDescription CanonicalASBD(Float64 sampleRate, UInt32 channel);
+
 @interface AudioUnitViewController ()
 - (void)prepareAUGraph;
 @end
@@ -48,6 +51,8 @@
     DBGMSG(@"%s", __func__);
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [self prepareAUGraph];
 }
 
 - (void)viewDidUnload
@@ -82,6 +87,63 @@
 - (void)prepareAUGraph
 {
     DBGMSG(@"%s", __func__);
+    AUNode      remoteIONode;
+    AudioUnit   remoteIOUnit;
+    
+    NewAUGraph(&__auGraph);
+    AUGraphOpen(self.auGraph);
+    
+    AudioComponentDescription   cd;
+    cd.componentType            = kAudioUnitType_Output;
+    cd.componentSubType         = kAudioUnitSubType_RemoteIO;
+    cd.componentManufacturer    = kAudioUnitManufacturer_Apple;
+    cd.componentFlags           = 0;
+    cd.componentFlagsMask       = 0;
+    
+    AUGraphAddNode(self.auGraph, &cd, &remoteIONode);
+    AUGraphNodeInfo(self.auGraph, remoteIONode, NULL, &remoteIOUnit);
+    
+    UInt32  flag = 1;
+    AudioUnitSetProperty(remoteIOUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Input, 1, &flag, sizeof(flag));
+    
+    AudioStreamBasicDescription audioFormat = CanonicalASBD(44100.0, 1);
+    AudioUnitSetProperty(remoteIOUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 1, &audioFormat, sizeof(AudioStreamBasicDescription));
+    AudioUnitSetProperty(remoteIOUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &audioFormat, sizeof(AudioStreamBasicDescription));
+    
+    AUGraphConnectNodeInput(self.auGraph, remoteIONode, 1, remoteIONode, 0);
+    AUGraphInitialize(self.auGraph);
 }
 
 @end
+
+static AudioStreamBasicDescription AUCanonicalASBD(Float64 sampleRate, UInt32 channel)
+{
+    AudioStreamBasicDescription audioFormat;
+    audioFormat.mSampleRate         = sampleRate;
+    audioFormat.mFormatID           = kAudioFormatLinearPCM;
+    audioFormat.mFormatFlags        = kAudioFormatFlagsAudioUnitCanonical;
+    audioFormat.mChannelsPerFrame   = channel;
+    audioFormat.mBytesPerPacket     = sizeof(AudioUnitSampleType);
+    audioFormat.mBytesPerFrame      = sizeof(AudioUnitSampleType);
+    audioFormat.mFramesPerPacket    = 1;
+    audioFormat.mBitsPerChannel     = 8 * sizeof(AudioUnitSampleType);
+    audioFormat.mReserved           = 0;
+    return audioFormat;
+}
+
+static AudioStreamBasicDescription CanonicalASBD(Float64 sampleRate, UInt32 channel)
+{
+    AudioStreamBasicDescription audioFormat;
+    audioFormat.mSampleRate         = sampleRate;
+    audioFormat.mFormatID           = kAudioFormatLinearPCM;
+    audioFormat.mFormatFlags        = kAudioFormatFlagsCanonical;
+    audioFormat.mChannelsPerFrame   = channel;
+    audioFormat.mBytesPerPacket     = sizeof(AudioSampleType) * channel;
+    audioFormat.mBytesPerFrame      = sizeof(AudioSampleType) * channel;
+    audioFormat.mFramesPerPacket    = 1;
+    audioFormat.mBitsPerChannel     = 8 * sizeof(AudioSampleType);
+    audioFormat.mReserved           = 0;
+    return audioFormat;
+}
+
+/* End Of File */
